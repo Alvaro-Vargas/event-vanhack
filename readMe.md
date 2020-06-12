4aa37e273803bfcd5ed2589a98274188427a3544
alvaro-vargas <alvaro.pvj@gmail.com>
2020-06-12 07:05:10 -0400

    feat: first commit

---
+++ readMe.md
@@ -0,0 +1,15 @@
+### Functionality (65%)
+- Apply to events and see confirmation of the application
+- See more details of the event
+- Share the event with a friend (through Linkedin, Facebook, Twitter, etc)
+- See a error warning when trying to apply for a premium only webnar.
+  - This error has to show an button for the candidate to read more about premium/buy a plan
+- Types of events:
+  - MeetUp, Leap, Recruiting Mission, VanHackaton
+  - Premium Only Webinar, Open Webinar
+
+
+### Design (35%)
+ - Leap, Mission and VanHackaton events should **visually stand out**
+ - Premium-only should be visually distinguished form the "open for all" webinars;
+ - Should not be a copu of the VanHack current events page.
---
+++ scripts.js
@@ -0,0 +1,23 @@
+const events = [
+  {
+    title: 'First Event',
+    // date: 'June 11, 2020',
+    date: {
+      day: 11,
+      month: 'June'
+    },
+    location: 'Online',
+    deadline: 'June 09, 2020',
+    image: 'https://images.unsplash.com/photo-1520110120835-c96534a4c984?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1347&q=80',
+    description: 'Culpa anim minim exercitation in cillum.  Deserunt laboris ad est nisi qui eu ipsum magna nostrud nisi nisi cupidatat.',
+    type: 'Leap, Mission and VanHackaton',
+    premium: true,
+  },
+]
+
+/**
+ * Images for later
+ * https://images.unsplash.com/photo-1573497019414-e44d0759d00e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60
+ */
+
+// var card = document.createElement()
